a2df8dcfdc9ee4a6635046e762fcb40bd8d04a6c
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-02-12 22:15:02 +0300

    dz.login

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ DZ.init({
     appId  : 'YOUR_APP_ID',
     channelUrl : 'http://YOUR_DOMAIN/channel.html'
 });
+
+DZ.login(function(response) {
+	if (response.authResponse) {
+		console.log('Welcome!  Fetching your information.... ');
+		DZ.api('/user/me', function(response) {
+			console.log('Good to see you, ' + response.name + '.');
+		});
+	} else {
+		console.log('User cancelled login or did not fully authorize.');
+	}
+}, {perms: 'basic_access,email'});
