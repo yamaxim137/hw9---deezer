f2e6beb9643c4e913c386c13f534874d9127d061
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-02-18 17:20:01 +0300

    не работает (

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-DZ.init({
-    appId  : 'YOUR_APP_ID',
-    channelUrl : 'http://YOUR_DOMAIN/channel.html'
+document.addEventListener('DOMContentLoaded', f137);
+
+
+f137 = DZ.init({
+    appId  : '395764',
+    channelUrl : 'https://my137.com',
+	// player : {
+	// 	onload : function(){}
+	// }
+	
+	
+	player: {
+		// container: 'player',
+		container: 'dz-root',
+		width : 800,
+		height : 300,
+		onload : function(){new PlayerSearch('#search');}
+	}
 });
 
-DZ.login(function(response) {
-	if (response.authResponse) {
-		console.log('Welcome!  Fetching your information.... ');
-		DZ.api('/user/me', function(response) {
-			console.log('Good to see you, ' + response.name + '.');
-		});
-	} else {
-		console.log('User cancelled login or did not fully authorize.');
-	}
-}, {perms: 'basic_access,email'});
+// DZ.login(function(response) {
+// 	if (response.authResponse) {
+// 		console.log('Welcome!  Fetching your information.... ');
+// 		DZ.api('/user/me', function(response) {
+// 			console.log('Good to see you, ' + response.name + '.');
+// 		});
+// 	} else {
+// 		console.log('User cancelled login or did not fully authorize.');
+// 	}
+// }, {perms: 'basic_access,email'});
+
